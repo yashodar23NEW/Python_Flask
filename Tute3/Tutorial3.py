c441b904db584091f7ac5c8ecec01480f9704b91
Ruwanthi Yashodara <119606787+yashodar23NEW@users.noreply.github.com>
2025-02-02 17:25:00 +0530

    Create Tutorial3.py

---
+++ Tute3/Tutorial3.py
@@ -0,0 +1,14 @@
+from flask import Flask,redirect,url_for,render_template
+
+app=Flask(__name__)
+@app.route("/home")
+def home():
+
+    return render_template("child.html")
+
+
+
+
+
+if __name__=="__main__":
+    app.run(debug=True)
